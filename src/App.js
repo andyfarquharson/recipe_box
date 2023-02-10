e13c7c6cb6b4929d1f4f5b079cc911382e708e77
Andy Farquharson <andyfarquharson@gmail.com>
2023-02-09 21:17:44 -0800

    Create form for adding recipes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: "",
+    instructions: "",
+  });
+  const handleChange = (event) => {
+    setRecipe({
+      ...recipe,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // add code to send the recipe to database
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={recipe.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Ingredients:
+        <input
+          type="text"
+          name="ingredients"
+          value={recipe.ingredients}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Instructions:
+        <textarea
+          name="instructions"
+          value={recipe.ingredients}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Save Recipe</button>
+    </form>
   );
 }
 
